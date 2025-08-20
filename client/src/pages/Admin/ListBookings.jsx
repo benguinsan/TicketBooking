e099b972a420df137a47b85251f1e82c003af073
Nguyen Phuc <penguinsan@Nguyens-MBP-3.lan>
2025-08-20 11:33:16 +0700

    completed 1/2 addShows

--- client/src/pages/Admin/ListBookings.jsx
+++ client/src/pages/Admin/ListBookings.jsx
@@ -3,6 +3,7 @@ import { dummyBookingData } from '../../assets/assets'
 import { useState, useEffect } from 'react'
 import Loading from '../../components/Loading'
 import Title from '../../components/Admin/Title'
+import dateFormat from '../../lib/dateFormat'
 
 const ListBookings = () => {
   const currency = import.meta.env.VITE_CURRENCY
@@ -22,6 +23,32 @@ const ListBookings = () => {
     return !isLoading ? (
         <>
             <Title textFirst='List' textLast='Bookings'/>
+            <div className='max-w-4xl mt-6 overflow-x-auto'>
+                <table className='w-full border-collapse rounded-md overflow-hidden text-nowrap'>
+                    {/* Table Header */}
+                    <thead>
+                        <tr className='bg-primary/20 text-left text-white'>
+                            <th className='p-2 font-medium pl-5'>User Name</th>
+                            <th className='p-2 font-medium'>Movie Name</th>
+                            <th className='p-2 font-medium'>Show Time</th>
+                            <th className='p-2 font-medium'>Seats</th>
+                            <th className='p-2 font-medium'>Amount</th>
+                        </tr>
+                    </thead>
+                    {/* Table Body */}
+                    <tbody className='text-sm font-light'>
+                       {bookings.map((item, index) => (
+                        <tr key={index} className='border-b border-primary/10 bg-primary/5 even:bg-primary/10'>
+                            <td className='p-2 min-w-45 pl-5'>{item.user.name}</td>
+                            <td className='p-2 min-w-45'>{item.show.movie.title}</td>
+                            <td className='p-2 min-w-45'>{dateFormat(item.show.showDateTime)}</td>
+                            <td className='p-2 min-w-45'>{Object.keys(item.bookedSeats).map(seat => item.bookedSeats[seat]).join(", ")}</td>
+                            <td className='p-2 min-w-45'>{currency} {item.amount}</td>
+                        </tr>
+                       ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     ) : (
         <Loading />
