3ab6bde5768f509f75b95ab394381aabd88719d1
Nguyen Phuc <penguinsan@Nguyens-MacBook-Pro-3.local>
2025-08-26 14:59:20 +0700

    update uri db

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,5 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
-import connectDB from "../configs/db.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
