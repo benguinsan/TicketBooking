c1dd54b62bb1966815804f79267a7fadb13d561e
Nguyen Phuc <penguinsan@Nguyens-MacBook-Pro-3.local>
2025-08-26 15:39:32 +0700

    update db name

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on("connected", () => console.log("Connected to MongoDB"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickShow`);
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`);
     } catch (error) {
         console.log(error);
     }
